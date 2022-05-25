31d409eeefaafdf21c5335f5040d1870ca743c77
ving494 <79427061+ving494@users.noreply.github.com>
2022-05-25 11:15:49 -0700

    assignment2vinh

---
+++ js/script.js
@@ -0,0 +1,281 @@
+const objJson = [ 
+    {avatar:"https://randomuser.me/api/portraits/thumb/women/67.jpg",
+    name:"iboya vat",
+   email:"iboya.vat@example.com",
+      dateUser:"Joined 07/15/15",
+},{avatar:"https://randomuser.me/api/portraits/thumb/men/75.jpg",
+    name:"aapo niskanen",
+   email:"aapo.niskanen@example.com",
+      dateUser:"Joined 06/15/12",
+},{avatar:"https://randomuser.me/api/portraits/thumb/men/34.jpg",
+    name:"phillip cox",
+   email:"phillip.cox@example.com",
+      dateUser:"Joined 09/11/14",
+},{avatar:"https://randomuser.me/api/portraits/thumb/women/38.jpg",
+    name:"zilda moreira",
+   email:"zilda.moreira@example.com",
+      dateUser:"Joined 07/15/15",
+},{avatar:"https://randomuser.me/api/portraits/thumb/women/23.jpg",
+    name:"lilou le gall",
+   email:"lilou.le gall@example.com",
+      dateUser:"Joined 06/16/13",
+},{avatar:"https://randomuser.me/api/portraits/thumb/women/94.jpg",
+    name:"lucy hall",
+   email:"lucy.hall@example.com",
+      dateUser:"Joined 09/11/16",
+},{avatar:"https://randomuser.me/api/portraits/thumb/women/24.jpg",
+    name:"mark colin",
+   email:"mark.colin@example.com",
+      dateUser:"Joined 01/14/14",
+},{avatar:"https://randomuser.me/api/portraits/thumb/women/14.jpg",
+    name:"sara alves",
+   email:"sara.alves@example.com",
+      dateUser:"Joined 07/19/16",
+},{avatar:"https://randomuser.me/api/portraits/thumb/men/49.jpg",
+    name:"ramon macrae",
+   email:"ramon.macrae@example.com",
+      dateUser:"Joined 05/13/12",
+},{avatar:"https://randomuser.me/api/portraits/thumb/men/15.jpg",
+    name:"connor taylor",
+   email:"connor.taylor@example.com",
+      dateUser:"Joined 05/18/14",
+},{avatar:"https://randomuser.me/api/portraits/thumb/men/11.jpg",
+    name:"aymeric morel",
+   email:"aymeric.morel@example.com",
+      dateUser:"Joined 06/13/13",
+},{avatar:"https://randomuser.me/api/portraits/thumb/men/49.jpg",
+    name:"lorenz otto",
+   email:"lorenz.otto@example.com",
+      dateUser:"Joined 05/11/14",
+},{avatar:"https://randomuser.me/api/portraits/thumb/men/29.jpg",
+    name:"karl williamson",
+   email:"karl.williamson@example.com",
+      dateUser:"Joined 01/12/14",
+},{avatar:"https://randomuser.me/api/portraits/thumb/men/34.jpg",
+    name:"ouassim heering",
+   email:"ouassim.heering@example.com",
+      dateUser:"Joined 01/18/12",
+},{avatar:"https://randomuser.me/api/portraits/thumb/men/91.jpg",
+    name:"roberto molina",
+   email:"roberto.molina@example.com",
+      dateUser:"Joined 06/13/15",
+},{avatar:"https://randomuser.me/api/portraits/thumb/men/28.jpg",
+    name:"jordan hubert",
+   email:"jordan.hubert@example.com",
+      dateUser:"Joined 06/13/15",
+},{avatar:"https://randomuser.me/api/portraits/thumb/men/29.jpg",
+    name:"melvin baker",
+   email:"melvin.baker@example.com",
+      dateUser:"Joined 09/18/14",
+},{avatar:"https://randomuser.me/api/portraits/thumb/men/26.jpg",
+    name:"everett gordon",
+   email:"everett.gordon@example.com",
+      dateUser:"Joined 06/17/15",
+},{avatar:"https://randomuser.me/api/portraits/thumb/men/82.jpg",
+    name:"aiden ma",
+   email:"aiden.ma@example.com",
+      dateUser:"Joined 07/18/12",
+},{avatar:"https://randomuser.me/api/portraits/thumb/men/62.jpg",
+    name:"florent gerard",
+   email:"florent.gerard@example.com",
+      dateUser:"Joined 02/12/13",
+},{avatar:"https://randomuser.me/api/portraits/thumb/women/96.jpg",
+    name:"amber chen",
+   email:"amber.chen@example.com",
+      dateUser:"Joined 07/12/12",
+},{avatar:"https://randomuser.me/api/portraits/thumb/women/9.jpg",
+    name:"alexandra davies",
+   email:"alexandra.davies@example.com",
+      dateUser:"Joined 05/11/13",
+},{avatar:"https://randomuser.me/api/portraits/thumb/men/57.jpg",
+    name:"sergio cole",
+   email:"sergio.cole@example.com",
+      dateUser:"Joined 02/17/15",
+},{avatar:"https://randomuser.me/api/portraits/thumb/men/98.jpg",
+    name:"edgar dixon",
+   email:"edgar.dixon@example.com",
+      dateUser:"Joined 06/17/11",
+},{avatar:"https://randomuser.me/api/portraits/thumb/men/94.jpg",
+    name:"kirk myers",
+   email:"kirk.myers@example.com",
+      dateUser:"Joined 09/17/15",
+},{avatar:"https://randomuser.me/api/portraits/thumb/women/28.jpg",
+    name:"ani hesseling",
+   email:"ani.hesseling@example.com",
+      dateUser:"Joined 08/14/16",
+},{avatar:"https://randomuser.me/api/portraits/thumb/women/24.jpg",
+    name:"victoire bonnet",
+   email:"victoire.bonnet@example.com",
+      dateUser:"Joined 05/13/16",
+},{avatar:"https://randomuser.me/api/portraits/thumb/men/44.jpg",
+    name:"marcos morales",
+   email:"marcos.morales@example.com",
+      dateUser:"Joined 01/12/12",
+},{avatar:"https://randomuser.me/api/portraits/thumb/men/75.jpg",
+    name:"nils neumann",
+   email:"nils.neumann@example.com",
+      dateUser:"Joined 03/11/12",
+},{avatar:"https://randomuser.me/api/portraits/thumb/women/86.jpg",
+    name:"emily harrison",
+   email:"emily.harrison@example.com",
+      dateUser:"Joined 07/18/15",
+},{avatar:"https://randomuser.me/api/portraits/thumb/men/41.jpg",
+    name:"matthew fortin",
+   email:"matthew.fortin@example.com",
+      dateUser:"Joined 03/18/15",
+},{avatar:"https://randomuser.me/api/portraits/thumb/women/32.jpg",
+    name:"charlotte steward",
+   email:"charlotte.steward@example.com",
+      dateUser:"Joined 02/18/11",
+},{avatar:"https://randomuser.me/api/portraits/thumb/men/3.jpg",
+    name:"marceau rodriguez",
+   email:"marceau.rodriguez@example.com",
+      dateUser:"Joined 07/13/14",
+},{avatar:"https://randomuser.me/api/portraits/thumb/men/91.jpg",
+    name:"hudson anderson",
+   email:"hudson.anderson@example.com",
+      dateUser:"Joined 09/12/15",
+},{avatar:"https://randomuser.me/api/portraits/thumb/men/33.jpg",
+    name:"warren phillips",
+   email:"warren.phillips@example.com",
+      dateUser:"Joined 01/11/12",
+},{avatar:"https://randomuser.me/api/portraits/thumb/men/41.jpg",
+    name:"leo niva",
+   email:"leo.niva@example.com",
+      dateUser:"Joined 09/14/15",
+},{avatar:"https://randomuser.me/api/portraits/thumb/men/3.jpg",
+    name:"hani prevoo",
+   email:"hani.prevoo@example.com",
+      dateUser:"Joined 01/11/15",
+},{avatar:"https://randomuser.me/api/portraits/thumb/women/79.jpg",
+    name:"veronica rodriguez",
+   email:"veronica.rodriguez@example.com",
+      dateUser:"Joined 07/17/12",
+},{avatar:"https://randomuser.me/api/portraits/thumb/men/41.jpg",
+    name:"ginestal das neves",
+   email:"ginestal.das neves@example.com",
+      dateUser:"Joined 06/19/12",
+},{avatar:"https://randomuser.me/api/portraits/thumb/men/63.jpg",
+    name:"devon barnes",
+   email:"devon.barnes@example.com",
+      dateUser:"Joined 09/19/14",
+},{avatar:"https://randomuser.me/api/portraits/thumb/men/55.jpg",
+    name:"brennan pierce",
+   email:"brennan.pierce@example.com",
+      dateUser:"Joined 05/15/14",
+},{avatar:"https://randomuser.me/api/portraits/thumb/men/35.jpg",
+    name:"zachary singh",
+   email:"zachary.singh@example.com",
+      dateUser:"Joined 05/19/11",
+},{avatar:"https://randomuser.me/api/portraits/thumb/men/35.jpg",
+    name:"arlo harris",
+   email:"arlo.harris@example.com",
+      dateUser:"Joined 07/12/15",
+},{avatar:"https://randomuser.me/api/portraits/thumb/women/60.jpg",
+    name:"hannah ginnish",
+   email:"hannah.ginnish@example.com",
+      dateUser:"Joined 05/17/14",
+},{avatar:"https://randomuser.me/api/portraits/thumb/men/95.jpg",
+    name:"goos brunt",
+   email:"goos.brunt@example.com",
+      dateUser:"Joined 09/15/12",
+},{avatar:"https://randomuser.me/api/portraits/thumb/men/91.jpg",
+    name:"eduard riedel",
+   email:"eduard.riedel@example.com",
+      dateUser:"Joined 06/12/15",
+},{avatar:"https://randomuser.me/api/portraits/thumb/women/42.jpg",
+    name:"geesken jekel",
+   email:"geesken.jekel@example.com",
+      dateUser:"Joined 02/12/13",
+},{avatar:"https://randomuser.me/api/portraits/thumb/women/54.jpg",
+    name:"dolores ryan",
+   email:"dolores.ryan@example.com",
+      dateUser:"Joined 06/17/16",
+},{avatar:"https://randomuser.me/api/portraits/thumb/men/49.jpg",
+    name:"steven rogers",
+   email:"steven.rogers@example.com",
+      dateUser:"Joined 01/18/11",
+},{avatar:"https://randomuser.me/api/portraits/thumb/men/63.jpg",
+    name:"virgulino silva",
+   email:"virgulino.silva@example.com",
+      dateUser:"Joined 03/18/11",
+},{avatar:"https://randomuser.me/api/portraits/thumb/women/11.jpg",
+    name:"lucile bertrand",
+   email:"lucile.bertrand@example.com",
+      dateUser:"Joined 06/13/16",
+},{avatar:"https://randomuser.me/api/portraits/thumb/women/10.jpg",
+    name:"elsa lahti",
+   email:"elsa.lahti@example.com",
+      dateUser:"Joined 04/15/12",
+},{avatar:"https://randomuser.me/api/portraits/thumb/women/12.jpg",
+    name:"soline leclercq",
+   email:"soline.leclercq@example.com",
+      dateUser:"Joined 05/12/14",
+},{avatar:"https://randomuser.me/api/portraits/thumb/men/89.jpg",
+    name:"henri kruse",
+   email:"henri.kruse@example.com",
+      dateUser:"Joined 05/14/13",}
+];
+var current_page = 1;
+var records_per_page = 10;
+
+function prevPage()
+{
+    if (current_page > 1) {
+        current_page--;
+        changePage(current_page);
+    }
+}
+
+function nextPage()
+{
+    if (current_page < 54) {
+        current_page++;
+        changePage(current_page);
+    }
+}
+    
+function changePage(page)
+{
+    var btn_next = document.getElementById("btn_next");
+    var btn_prev = document.getElementById("btn_prev");
+    var listing_table = document.getElementById("listingTable");
+    var page_span = document.getElementById("page");
+ 
+    // Validate page
+    if (page < 1) page = 1;
+    if (page > 54) page = 54;
+
+    listing_table.innerHTML = `<ul class="contact-list">`;
+
+    for (var i = (page-1) * records_per_page; i < (page * records_per_page); i++) {
+        listing_table.innerHTML += `
+        <li class="contact-item cf">
+            <div class="contact-details">
+                <img class="avatar" src="${objJson[i].avatar}">
+                <h3>${objJson[i].name}</h3>
+                <span class="email">${objJson[i].email}</span>
+            </div>
+            <div class="joined-details">
+                <span class="date">${objJson[i].dateUser}</span>
+        </div>
+        </li>
+        `;
+    }
+    page_span.innerHTML = page;
+
+    if (page == 1) {
+        btn_prev.style.visibility = "hidden";
+    } else {
+        btn_prev.style.visibility = "visible";
+    }
+
+    if (page == 54) {
+        btn_next.style.visibility = "hidden";
+    } else {
+        btn_next.style.visibility = "visible";
+    }
+}
+window.onload = function() {
+    changePage(1);
+};
